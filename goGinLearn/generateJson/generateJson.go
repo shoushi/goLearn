51d7306f53a981feab4d8873999e0b8f235d1151
guanliqun1 <guanliqun@geekplus.com>
2023-04-04 20:03:01 +0800

    报文修改

--- goGinLearn/generateJson/generateJson.go
+++ goGinLearn/generateJson/generateJson.go
@@ -2,28 +2,35 @@ package main
 
 import (
 	"io/ioutil"
+	"os"
 	"strconv"
 	"strings"
 )
 
-func generateJson(count int) string {
-	res, _ := ioutil.ReadFile("./报文生成/resource.json")
-	t1, _ := ioutil.ReadFile("./报文生成/template.json")
+func generateJson(template string, resource string, count int, page int) string {
 	json := ""
-	template := string(t1)
-	for i := 0; i < count; i++ {
+	for i := page * count; i < count*(page+1); i++ {
 		temp := strings.ReplaceAll(template, "{num}", strconv.Itoa(i))
 		if i == 0 {
 			json = temp
 		} else {
 			json = json + "," + temp
 		}
+		println("now is " + strconv.Itoa(i))
 	}
-	resStr := strings.ReplaceAll(string(res), "{template}", json)
-	ioutil.WriteFile("./报文生成/result.json", []byte(resStr), 0664)
-	return resStr
+	resStr := strings.ReplaceAll(resource, "{template}", json)
+	os.Create("./报文生成/result" + strconv.Itoa(page) + ".json")
+	ioutil.WriteFile("./报文生成/result"+strconv.Itoa(page)+".json", []byte(resStr), 0664)
+	return json
 }
 
 func main() {
-	generateJson(2000)
+	res1, _ := ioutil.ReadFile("./报文生成/resource.json")
+	t1, _ := ioutil.ReadFile("./报文生成/template.json")
+	template := string(t1)
+	resource := string(res1)
+	for i := 1; i < 99; i++ {
+		go generateJson(template, resource, 100000, i)
+	}
+	generateJson(template, resource, 200000, 0)
 }
