b9e7d15e8a2f0c7e58d579eb19aad6b7e3e84e39
guanliqun1 <guanliqun@geekplus.com>
2023-07-26 17:53:07 +0800

    修改mock支持关键字替换

--- goGinLearn/mock.go
+++ goGinLearn/mock.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
+	"strconv"
 	"strings"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -12,7 +15,10 @@ func postMock(fileName string, group *gin.RouterGroup, dirPath string) {
 	urlPath := strings.ReplaceAll(fileName, "_", "/")
 	urlPath = strings.Split(urlPath, ".")[0]
 	group.POST(urlPath, func(ctx *gin.Context) {
-		ctx.File(dirPath + fileName)
+
+		// ctx.File(dirPath + fileName)
+		ctx.Header("Content-Type", "application/json")
+		ctx.String(200, generateMsg(dirPath+fileName))
 	})
 }
 func mock(mockRout *gin.Engine, groupPath string, dirPath string) {
@@ -25,4 +31,15 @@ func mock(mockRout *gin.Engine, groupPath string, dirPath string) {
 			postMock(file.Name(), group, dirPath)
 		}
 	}
+}
+
+func generateMsg(fileName string) string {
+	file, _ := os.OpenFile(fileName, os.O_RDWR, 0644)
+	content, _ := ioutil.ReadAll(file)
+	defer file.Close()
+
+	currentTime := time.Now().Unix()
+	timestampStr := strconv.FormatInt(currentTime, 10)
+	newContent := strings.ReplaceAll(string(content), "${timestemp}", timestampStr)
+	return newContent
 }
